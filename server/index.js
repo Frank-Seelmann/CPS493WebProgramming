8e3c9b12ff808a6ece17e79b519cd35d4c306ad9
frankdaseel <frankseelmann123@gmail.com>
2021-09-23 15:18:47 -0400

    added express

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+/*
+const { SSL_OP_EPHEMERAL_RSA } = require('constants');
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -6,9 +8,28 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.write("Going");
+  res.end('You asked for ' + req.url);
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
+//----------------------------------//
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app
+.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+.get('/newpaltz', (req,res) => {
+  res.send('Hello New Paltz!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
