baab8d4b0b745ecb7cd6c2ef5f8d74f71dc872ba
frankdaseel <frankseelmann123@gmail.com>
2021-10-21 14:54:34 -0400

    working* API

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
-const express = require('express')
+const express = require('express');
+const usersController = require('./controllers/users')
+
 const app = express()
 const port = 3000
 
 app
-.get('/', (req, res) => {
-  res.send('Hello World!')
+.get('*', (req, res, next)=>{
+  console.log('A request came in.');
+  next();
+})
+.get('/', (req, res) => { //these are requests
+  res.send('Hello World!') //send is a terminal request
 })
 .get('/newpaltz', (req,res) => {
   res.send('Hello New Paltz!!')
 })
+.use('/users', usersController)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
