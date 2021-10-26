a11ff2eacf2ea2273d0a8ea7c799bafd23157935
frankdaseel <frankseelmann123@gmail.com>
2021-10-26 19:45:22 -0400

    fleshing out our controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,14 @@
+/*  B"H
+*/
 const express = require("express");
-const model = require("../models/users")
-
+const model = require("../models/users");
 const app = express.Router();
-
 app
-    .get("/", (req, res, next) => {
+    .get("/", (req, res, next) =>{
         res.send(model.GetAll());
     })
+    .get("/:user_id", (req, res, next) =>{
+        res.send(model.Get(req.params.user_id));
+    })
 
-    module.exports = app;
+module.exports = app; 
--- server/index.js
+++ server/index.js
@@ -4,17 +4,18 @@ require('dotenv').config();
 console.log(`The best class at New Paltz is ${process.env.BEST_CLASS}`);
 
 const usersController = require('./controllers/users');
+const postsController = require('./controllers/posts');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app
-  .use('/', express.static(path.join(__dirname, '../docs'))) //static returns a function that checks for the route
-  .use('/users', usersController)
+    .use('/', express.static(path.join(__dirname, '../docs')) )
+    .use('/users', usersController )
+    .use('/posts', postsController)
 
 app
-  .get('*', (req,res) => res.sendFile(path.join(__dirname, '../docs/index.html')) )
-
+    .get('*', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')) )
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
