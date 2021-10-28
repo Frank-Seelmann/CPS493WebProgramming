30c275e18dfe6c53ab4fd59a054ece1b8a899ac6
frankdaseel <frankseelmann123@gmail.com>
2021-10-28 15:21:08 -0400

    allowing posts

--- server/index.js
+++ server/index.js
@@ -11,11 +11,19 @@ const port = process.env.PORT || 3000;
 
 app
     .use('/', express.static(path.join(__dirname, '../docs')) )
+
+    .use(express.json()) //no path means everything
     .use('/users', usersController )
     .use('/posts', postsController)
 
 app
     .get('*', (req, res) => res.sendFile(path.join(__dirname, '../docs/index.html')) )
+
+app
+    .use((err, req, res, next) => {
+      res.status(err.code || 500).send(err);
+    })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
