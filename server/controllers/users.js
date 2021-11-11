12fe9bcde63960ef64b39fc1286a52b2e612406c
frankdaseel <frankseelmann123@gmail.com>
2021-11-11 14:39:18 -0500

    caught up

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,52 +2,60 @@
 */
 const express = require("express");
 const model = require("../models/users");
-const app = express.Router();
-app
-    /*.get("/async", (req, res, next) => {
-        console.log("Outer function: 1")
-        model.Async();
-        res.send("Done!");
-        console.log("Outer function: 2")
-    })*/
-    .get("/", (req, res, next) => {
-        model.GetAll()
-        .then(user => {
-            res.send(user);
-        })
-        .catch(next)
-    })
-    .get("/:user_id", (req, res, next) => {
-        model.Get(req.params.user_id)
-        .then(user => {
-            res.send(user);
-        })
-        .catch(next)
-    })
-    .post("/login", (req, res, next) => {
-        //const user = model.Login(req.body.handle,req.body.password);
-        //res.send(user);
 
-        model.Login(req.body.handle, req.body.password)
-            .then(user => {
+const app = express.Router();
+
+app
+    .get("/", (req, res, next) =>{
+        model.GetAll()
+        .then(user=>{ 
+            res.send(user);
+        })
+        .catch(next) 
+    })
+    .get("/:user_id", (req, res, next) =>{
+         model.Get(req.params.user_id)
+            .then(user=>{ 
                 res.send(user);
             })
-            .catch(next)
+            .catch(next) 
     })
+    .patch("/:user_id", (req, res, next) =>{
 
-    .post("/register", (req, res, next) => {
+        model   .Update(req.params.user_id, req.body)
+                .then( user=> res.send(user) )
+                .catch(next) 
+
+    })
+    .delete("/:user_id", (req, res, next) =>{
+
+        model   .Delete(req.params.user_id)
+                .then( user=> res.send({ deleted: user }) )
+                .catch(next) 
+
+    })
+    .post("/login", (req, res, next) =>{
+
+        model.Login(req.body.handle, req.body.password)
+            .then(user=>{ 
+                res.send(user);
+            })
+            .catch(next) 
+
+    })
+    .post("/register", (req, res, next) =>{
         model.Add(req.body)
-            .then(user => {
+            .then(user=>{
                 res.status(201).send(user);
             })
-            .catch(next)
+            .catch(next) 
     })
-    .post("/seed", (req, res, next) => {
+    .post("/seed", (req, res, next) =>{
         model.Seed()
-            .then(user => {
+            .then(user=>{
                 res.status(201).send("Created");
             })
-            .catch(next)
+            .catch(next) 
     })
 
 module.exports = app;
